4ab972627d52136bbdebef527b8488a17ac2198b
NariaNexis <94880088+NariaNexis@users.noreply.github.com>
2023-04-28 17:22:15 -0700

    hello mario

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ for (const folder of commandFolders) {
 	}
 }
 
-
+//hello mario
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
